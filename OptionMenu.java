4d0d94325537b514d838139719be29252f7b62f9
Sk-Badsha <108122668+Sk-Badsha@users.noreply.github.com>
2022-07-08 17:24:50 +0530

    Atm

---
+++ OptionMenu.java
@@ -0,0 +1,12 @@
+package atm;
+
+import java.io.IOException;
+
+public class OptionMenu extends MainMenu {
+	public static void main(String[] args) throws IOException {
+		MainMenu mainmenu = new MainMenu();
+		mainmenu.getLogin();
+
+	}
+
+}
